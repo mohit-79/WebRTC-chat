e3c3494afe28a9b1978d6c5fb331b686b1733834
mohit-79 <mohitharjani79@gmail.com>
2026-07-05 19:53:48 +0530

    thunderclap sanity chec

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/health", (req, res) => res.json({ ok: true }));
+app.post("/echo", (req, res) => {
+    res.json({
+        youSent: req.body
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server on ${PORT}`));
 
-// console.log(process.env.PORT);
+console.log(process.env.PORT + "HELLO");
