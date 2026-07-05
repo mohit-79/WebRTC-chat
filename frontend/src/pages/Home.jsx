fa104fc16ce52ac009067832f680216372043bf9
mohit-79 <mohitharjani79@gmail.com>
2026-07-05 21:07:41 +0530

    react-router+axiosapi

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Home() {
-    return <h1>Home Page</h1>;
+
+    const [status, setStatus] = useState("");
+
+    useEffect(() => {
+
+        api.get("/health")
+            .then((res) => {
+                setStatus(res.data.ok ? "Backend Connected ✅" : "Failed");
+            })
+            .catch(() => {
+                setStatus("Backend Not Reachable ❌");
+            });
+
+    }, []);
+
+    return (
+        <>
+            <h1>Home Page</h1>
+            <p>{status}</p>
+        </>
+    );
 }
 
 export default Home;
