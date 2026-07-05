b2b3715baeea7eb777bb24181eea96ad2c022d5c
mohit-79 <mohitharjani79@gmail.com>
2026-07-05 20:46:34 +0530

    react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return (
-    <h1 className="text-5xl font-bold text-blue-600">
-      WebRTC-chat
-    </h1>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<Home />} />
+
+                <Route path="/login" element={<Login />} />
+
+                <Route path="*" element={<NotFound />} />
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
