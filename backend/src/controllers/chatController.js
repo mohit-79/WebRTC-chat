23ecd6b48a34827686ac274ecccbee932e80aa3a
mohit-79 <mohitharjani79@gmail.com>
2026-07-09 21:05:38 +0530

    pagination using beforeand limit params in request body

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -20,12 +20,28 @@ const createMessage = async (req, res) => {
 // GET /chat/:room
 const getMessages = async (req, res) => {
     try {
-
-        const messages = await Message.find({
-            room: req.params.room
-        }).sort({
-            createdAt: 1
-        });
+        const room = req.params.room;
+        const before = req.query.before;
+        const limit = parseInt(req.query.limit) || 20;
+        //query obj
+        const query = {
+            room: room
+        };
+        // modify query
+        if(before){
+            query._id = {
+                $lt: before
+            };
+        }
+        // const messages = await Message.find({
+        //      room: req.params.room
+           
+        // }).sort({
+            // createdAt: 1
+        // });
+        const messages = await Message.find(query)
+        .sort({ _id: -1 })
+        .limit(limit);
 
         res.json(messages);
 
